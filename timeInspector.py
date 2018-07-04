02c11f21cde587f9aaf50d1d14cb04b1bec640c5
lcwyo <lancechat@yahoo.com>
2018-07-04 17:58:57 +0200

    got the lunch inspector kind of running.

--- timeInspector.py
+++ timeInspector.py
@@ -25,7 +25,7 @@ class TimeInspector(tk.Tk):
         filemenu = tk.Menu(self.menu)
         filemenu.add_command(label='Settings', command=lambda: self.show_frame(SettingsPage))
         filemenu.add_command(label='Time Inspector 2.0', command=lambda: self.show_frame(MainPage))
-        filemenu.add_command(label='Pizza Inspector', command=lambda: self.show_frame(TestPage))
+        filemenu.add_command(label='Lunch Inspector', command=lambda: self.show_frame(LunchPage))
         filemenu.add_separator()
         filemenu.add_command(label='Exit', command=quit)
         self.menu.add_cascade(label='File', menu=filemenu)
@@ -42,7 +42,7 @@ class TimeInspector(tk.Tk):
 
         self.frames = {}
 
-        for F in (TestPage, AboutPage, MainPage, HelpPage, KmfPage, SettingsPage):
+        for F in (TestPage, AboutPage, MainPage, HelpPage, KmfPage, SettingsPage, LunchPage):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=1, sticky="nsew")
@@ -249,6 +249,110 @@ class MainPage(tk.Frame):
             extraTime = (diff - timedelta(hours=8, minutes=50))
             self.timeLeft_display_label.set(str(extraTime)[:-3])
             self.timeLeft_display_text.set("Overtime earned")
+
+
+class LunchPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        text_frame = tk.Frame(self, bd=0, relief="groove")
+        time_frame = tk.Frame(self, bd=0, relief="groove", padx=2)
+        button_frame = tk.Frame(self, bd=0, relief="sunken", bg="white")
+        image_frame = tk.Frame(self, bd=0, relief="groove")
+
+        text_frame.grid(row=1, column=0, sticky="ew")
+        time_frame.grid(row=1, column=1, sticky="ew")
+        button_frame.grid(row=6, column=0, columnspan=6, padx=2)
+        image_frame.grid(row=0, column=3, rowspan=6, sticky="ew")
+
+        self.currentTime = tk.Label(text_frame, text='Current Time')
+        self.currentTime_Display = tk.Label(time_frame)
+
+        self.startTime_text = tk.Label(text_frame, text='Start Time')
+        self.startTime_Entry = tk.Entry(time_frame, width=5)
+
+        self.start_lunch_time_display_label = tk.StringVar()
+        self.start_lunch_time_text = tk.Label(text_frame, text='Start Lunch Time')
+        self.start_lunch_entry = tk.Entry(time_frame, width=5)
+
+        self.end_lunch_time_display_label = tk.StringVar()
+        self.end_lunch_time_text = tk.Label(text_frame, text='End Lunch Time')
+        self.end_lunch_entry = tk.Entry(time_frame, width=5)
+
+        self.goHome_display_label = tk.StringVar()
+        self.goHome_text = tk.Label(text_frame, text='You can leave the building at')
+        self.goHome_display = tk.Label(time_frame, textvariable=self.goHome_display_label)
+
+        button_ok = ttk.Button(button_frame, text="OK", command=self.lunch_ti)
+        button_close = ttk.Button(button_frame, text="Close", command=quit)
+
+        self.bind('<Return>', self.lunch_ti)
+
+        self.currentTime.grid(row=0, column=0, sticky='nw')
+        self.currentTime_Display.grid(row=0, column=1, sticky='nw')
+
+        self.startTime_text.grid(row=1, column=0, sticky='nw')
+        self.startTime_Entry.grid(row=1, column=1, sticky='nw')
+
+        self.start_lunch_time_text.grid(row=2, column=0, sticky='nw')
+        self.start_lunch_entry.grid(row=2, column=1, sticky='nw')
+
+        self.end_lunch_time_text.grid(row=3, column=0, sticky='nw')
+        self.end_lunch_entry.grid(row=3, column=1, sticky='nw')
+
+        self.goHome_text.grid(row=4, column=0, sticky='nw')
+        self.goHome_display.grid(row=4, column=1, sticky='nw')
+
+        button_ok.grid(row=1, column=0, sticky='nw')
+        button_close.grid(row=1, column=1, sticky='nw')
+        self.tick()
+
+        if tk.TkVersion >= 8.6:
+            self.extension = 'png'
+        else:
+            self.extension = 'ppm'
+
+        self.photo = tk.PhotoImage(file="./res/img/inspector." + self.extension)
+        self.image_label = tk.Label(image_frame, image=self.photo)
+        self.image_label.pack(side='top')
+
+    def tick(self, time1=''):
+        self.time1 = time1
+        # get the current local time from the PC
+        self.time2 = time.strftime('%H:%M')
+        # if time string has changed, update it
+        if self.time2 != self.time1:
+            self.time1 = self.time2
+            self.currentTime_Display.config(text=self.time2)
+        # calls itself every 200 milliseconds
+        # to update the time display as needed
+        self.currentTime_Display.after(200, self.tick)
+
+    def lunch_ti(self, event=None):
+        time_now = datetime.now().strftime('%H:%M')
+        
+        if self.startTime_Entry.get() != "":
+            time1 = datetime.strptime(self.startTime_Entry.get(), '%H:%M')
+        else:
+            time1 = datetime.strptime("8:00", '%H:%M')
+            self.startTime_Entry.insert(0, "08:00")
+
+        if self.start_lunch_entry.get() != "":
+            pTime1 = datetime.strptime(self.start_lunch_entry.get(), '%H:%M')
+        else:
+            pTime1 = datetime.strptime("12:00", '%H:%M')
+            self.start_lunch_entry.insert(0, "12:00")
+
+        if self.end_lunch_entry.get() != "":
+            pTime2 = datetime.strptime(self.end_lunch_entry.get(), '%H:%M')
+        else:
+            pTime2 = datetime.strptime("13:00", '%H:%M')
+            self.end_lunch_entry.insert(0, "13:00")
+
+        diff = pTime2 - pTime1
+        # this just makes the start time be whatever the break time was later
+        # ie if the break was 1:00 and start time was 8:00, it uses 9:00 as the start
+        # need to calculate the difference between the normal lunch and the extended lunch
+        self.goHome_display_label.set(go_home(time1 + diff))
 
 
 def go_home(start):
